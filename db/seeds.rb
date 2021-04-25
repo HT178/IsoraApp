e18b51da9ab41e83b85dc2ab06cffa9dd061d209
HT178 <hiratatomoya23@gmail.com>
2021-04-25 10:54:24 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-# CSV.foreach('db/result_seed.csv') do |row|
-#   Result.create(:user_id => row[0],
-#                     :level_id => row[1],
-#                     :word_id => row[2],
-#                     :practice_id => row[3])
-# end
+CSV.foreach('db/result_seed.csv') do |row|
+  Result.create(:user_id => row[0],
+                    :level_id => row[1],
+                    :word_id => row[2],
+                    :practice_id => row[3])
+end
 
-# CSV.foreach('db/word_seed.csv') do |row|
-#   Word.create(:word_id => row[0],
-#                     :word_en => row[1],
-#                     :word_jp => row[2],
-#                     :level_id => row[3],
-#                     :category_id => row[4],
-#                     :practice_id => row[5])
-# end
-
-# CSV.foreach('db/user_seed.csv') do |row|
-#   User.create(:user_id => row[0],
-#                     :password => row[1])
-# end
-
-
-
-# CSV.foreach('db/score_seed_0.csv') do |row|
-#   Score.create(:user_id => row[0],
-#                     :level_id => row[1])
-# end
-
-CSV.foreach('db/word_seed_0.csv') do |row|
+CSV.foreach('db/word_seed.csv') do |row|
   Word.create(:word_id => row[0],
                     :word_en => row[1],
                     :word_jp => row[2],
@@ -44,9 +23,12 @@ CSV.foreach('db/word_seed_0.csv') do |row|
                     :practice_id => row[5])
 end
 
-# CSV.foreach('db/result_seed_0.csv') do |row|
-#   Result.create(:user_id => row[0],
-#                     :level_id => row[1],
-#                     :word_id => row[2],
-#                     :practice_id => row[3])
-# end
+CSV.foreach('db/user_seed.csv') do |row|
+  User.create(:user_id => row[0],
+                    :password => row[1])
+end
+
+CSV.foreach('db/score_seed.csv') do |row|
+  Score.create(:user_id => row[0],
+                    :level_id => row[1])
+end
